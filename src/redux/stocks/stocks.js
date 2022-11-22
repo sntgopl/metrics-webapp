284a25dacce3c833f7d97d43bc8370988e9c00a8
sntgopl <santi.ponsle@gmail.com>
2022-11-22 20:03:58 -0300

    Add initial status

--- src/redux/stocks/stocks.js
+++ src/redux/stocks/stocks.js
@@ -1,11 +1,16 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-const GET_STOCKS = 'stocks/GET_STOCKS';
+export const getStocks = (payload) => (
+  {
+    type: 'GET_STOCKS',
+    payload
+  }
+);
 
 const initialState = [];
 
 const getStock = createAsyncThunk(GET_STOCKS, async () => {
-  const result = await fetch('https://financialmodelingprep.com/api/v3/stock/list?apikey=6622e5e4f7d591f15bccf177716f9851', () => {
+  const result = await fetch('https://financialmodelingprep.com/api/v3/stock/list?apikey=6622e5e4f7d591f15bccf177716f9851', {
     method: 'GET',
     headers: {
       'Content-type': 'application/json',
@@ -14,6 +19,7 @@ const getStock = createAsyncThunk(GET_STOCKS, async () => {
   const response = result.json();
   return response;
 });
+
 
 const stockReducer = (state = initialState, action) => {
   let list = [];
@@ -26,6 +32,8 @@ const stockReducer = (state = initialState, action) => {
         stock.price = element.price;
       });
       return list;
+    default:
+      return state
   }
 };
 

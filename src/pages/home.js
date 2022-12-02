193edadfb0b95107b463637506347efcb488aa48
sntgopl <105621110+sntgopl@users.noreply.github.com>
2022-12-02 13:05:57 -0300

    Add NavLink into details page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 import Stock from '../components/stocks';
 import { getStock } from '../redux/stocks/stocks';
 import './home.css';
@@ -16,12 +17,16 @@ const Home = () => {
   return (
     <div className="table">
       {stocks.map((stock) => (
-        <Stock
-          key={stock.symbol}
-          symbol={stock.symbol}
-          name={stock.name}
-          price={stock.price}
-        />
+        <NavLink key={stock.symbol} to={`/${stock.symbol}`}>
+          <div key={stock.symbol}>
+            <Stock
+              key={stock.symbol}
+              symbol={stock.symbol}
+              name={stock.name}
+              price={stock.price}
+            />
+          </div>
+        </NavLink>
       ))}
     </div>
   );

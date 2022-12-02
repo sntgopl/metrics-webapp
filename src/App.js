9f1b3ca5a76b31346af75817eefc2f0275ddda4f
sntgopl <105621110+sntgopl@users.noreply.github.com>
2022-12-02 13:05:23 -0300

    Import Brower Router dependencies

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/navbar';
 import Home from './pages/home';
+import Details from './components/details';
 import './App.css';
 
 function App() {
   return (
-    <div className="app">
-      <NavBar />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:id" element={<Details />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
